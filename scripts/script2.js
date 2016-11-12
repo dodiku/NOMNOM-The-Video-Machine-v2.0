e909daf9e4cfbcc0fa24f769f82c98bc62284d14
dodiku <ayalon.dror@gmail.com>
2016-11-12 12:05:33 -0500

    sounds are being played

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,5 +1,59 @@
-var part = new Tone.Part(
-	function(time, note){synth.triggerAttackRelease(note, "8n", time);},
-	[[0, "C2"], ["0:2", "C3"], ["0:3:2", "G2"]]
-).start(0);
-// comment
+var mySound, myPhrase, myPart;
+
+var msg = 'click to play';
+
+// function preload() {
+//   mySound = loadSound('assets/bd2.wav');
+// }
+
+// function preload() {
+//   mySound = loadSound('assets/bd.wav');
+// }
+
+function setup() {
+	createCanvas(200, 200);
+	mySound = loadSound("assets/bd2.wav", soundLoaded, soundError, soundLoading);
+
+
+	// configurations for the PHRASE << new p5.Phrase(name,callback,sequence) >>
+	var sequence = [1,3,0,2,0,1,0,0];
+	myPhrase = new p5.Phrase('bbox', makeSound, sequence);
+
+	// configurations for the PART
+  myPart = new p5.Part();
+  myPart.addPhrase(myPhrase);
+  myPart.setBPM(60);
+}
+var bla = 0;
+function makeSound(time, playbackRate) {
+  mySound.rate(playbackRate);
+  mySound.play(time);
+	bla++;
+	console.log('bla: ' + bla);
+	console.log('got it');
+}
+
+function draw() {
+  background(220);
+}
+
+// playing the PART
+function mouseClicked() {
+		myPart.loop();
+    // myPart.start();
+  }
+// myPart.start();
+
+// debug
+function soundError (){
+  console.log('there was a problem loading the tune...');
+}
+
+function soundLoading (value){
+  console.log('loading...');
+}
+
+function soundLoaded (value){
+	mySound.setVolume(0.1);
+	mySound.play();
+}
